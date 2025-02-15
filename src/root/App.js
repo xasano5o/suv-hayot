08026ac9833c2333d78424541780b24f16066069
xasano5o <xasandev@mail.ru>
2025-02-15 14:58:01 +0500

    f

--- src/root/App.js
+++ src/root/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import HomePageuser from "../page/home";
+import { BrowserRouter, Route, Routes } from "react-router";
+import HomeIdPage from "../components/ui/idPage";
 
 function App() {
   return (
     <div className="App">
-      <HomePageuser />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePageuser />} />
+          <Route path="/home/:id" element={<HomeIdPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
